3e811292176346f7d8add2a09af9a0a156519e16
myseneca-106280217 <113478957+myseneca-106280217@users.noreply.github.com>
2022-10-16 23:04:34 -0400

    Create file1.py

---
+++ file1.py
@@ -0,0 +1,11 @@
+"A function that takes an input from the user and calculate age of a person":
+  
+#!/usr/bin/env/python3 #sh-bang_line  
+
+#defining the age function which contains two variables and return the value of someone's age.
+def calculating_age():
+  Birth_Year = input("Insert your year of birth: ")
+  age = 2022-int(Birth_Year)
+  return f"Your current age is (age) years"
+
+print(calculating_age())
